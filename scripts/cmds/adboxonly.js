f9b5597017f56866f58d68a0af1f864d092f19cf
3voldi <voldigoanos65@gmail.com>
2025-11-28 22:29:09 +0000

    Update adboxonly.js

--- scripts/cmds/adboxonly.js
+++ scripts/cmds/adboxonly.js
@@ -7,25 +7,25 @@ module.exports = {
 		countDown: 5,
 		role: 1,
 		description: {
-			vi: "bật/tắt chế độ chỉ quản trị của viên nhóm mới có thể sử dụng bot",
+			fr: "activer/désactiver le mode où seul l'administrateur du groupe peut utiliser le bot",
 			en: "turn on/off only admin box can use bot"
 		},
-		category: "box chat",
+		category: "discussion de groupe",
 		guide: {
-			vi: "   {pn} [on | off]: bật/tắt chế độ chỉ quản trị viên nhóm mới có thể sử dụng bot"
-				+ "\n   {pn} noti [on | off]: bật/tắt thông báo khi người dùng không phải là quản trị viên nhóm sử dụng bot",
+			fr: "   {pn} [on | off] : activer/désactiver le mode où seul l'administrateur du groupe peut utiliser le bot"
+				+ "\n   {pn} noti [on | off] : activer/désactiver la notification lorsqu'un utilisateur non administrateur utilise le bot",
 			en: "   {pn} [on | off]: turn on/off the mode only admin of group can use bot"
 				+ "\n   {pn} noti [on | off]: turn on/off the notification when user is not admin of group use bot"
 		}
 	},
 
 	langs: {
-		vi: {
-			turnedOn: "Đã bật chế độ chỉ quản trị viên nhóm mới có thể sử dụng bot",
-			turnedOff: "Đã tắt chế độ chỉ quản trị viên nhóm mới có thể sử dụng bot",
-			turnedOnNoti: "Đã bật thông báo khi người dùng không phải là quản trị viên nhóm sử dụng bot",
-			turnedOffNoti: "Đã tắt thông báo khi người dùng không phải là quản trị viên nhóm sử dụng bot",
-			syntaxError: "Sai cú pháp, chỉ có thể dùng {pn} on hoặc {pn} off"
+		fr: {
+			turnedOn: "✅ Mode activé : seul l'administrateur du groupe peut utiliser le bot",
+			turnedOff: "❌ Mode désactivé : tous les membres peuvent utiliser le bot",
+			turnedOnNoti: "✅ Notification activée lorsqu'un utilisateur non administrateur utilise le bot",
+			turnedOffNoti: "❌ Notification désactivée lorsqu'un utilisateur non administrateur utilise le bot",
+			syntaxError: "⚠️ Erreur de syntaxe, utilisez uniquement {pn} on ou {pn} off"
 		},
 		en: {
 			turnedOn: "Turned on the mode only admin of group can use bot",
@@ -42,15 +42,15 @@ module.exports = {
 		let keySetData = "data.onlyAdminBox";
 		let indexGetVal = 0;
 
-		if (args[0] == "noti") {
+		if (args[0] === "noti") {
 			isSetNoti = true;
 			indexGetVal = 1;
 			keySetData = "data.hideNotiMessageOnlyAdminBox";
 		}
 
-		if (args[indexGetVal] == "on")
+		if (args[indexGetVal] === "on")
 			value = true;
-		else if (args[indexGetVal] == "off")
+		else if (args[indexGetVal] === "off")
 			value = false;
 		else
 			return message.reply(getLang("syntaxError"));
