228c350148a04bb7e29318174eccda6642f01d82
3voldi <voldigoanos65@gmail.com>
2025-11-29 10:05:09 +0000

    Update viponly.js

--- scripts/cmds/viponly.js
+++ scripts/cmds/viponly.js
@@ -7,28 +7,28 @@ module.exports = {
     name: "viponly",
     aliases: ["vipon", "onlyvip"],
     version: "1.0",
-    author: "NTKhang | Saimx69x",
+    author: "Christus",
     countDown: 5,
     role: 3,
     description: {
-      vi: "Bật/tắt chế độ chỉ VIP user mới có thể sử dụng bot",
+      fr: "Activer/désactiver le mode où seuls les utilisateurs VIP peuvent utiliser le bot",
       en: "Turn on/off only VIP users can use bot"
     },
     category: "owner",
     guide: {
-      vi: "   {pn} [on | off]: Bật/tắt chế độ chỉ VIP mới có thể sử dụng bot"
-        + "\n   {pn} noti [on | off]: Bật/tắt thông báo khi người dùng không phải VIP sử dụng bot",
+      fr: "   {pn} [on | off] : Activer/désactiver le mode VIP seulement"
+        + "\n   {pn} noti [on | off] : Activer/désactiver la notification lorsqu’un utilisateur non VIP utilise le bot",
       en: "   {pn} [on | off]: Turn on/off the mode only VIP can use bot"
         + "\n   {pn} noti [on | off]: Turn on/off the notification when user is not VIP use bot"
     }
   },
 
   langs: {
-    vi: {
-      turnedOn: "Đã bật chế độ chỉ VIP mới có thể sử dụng bot",
-      turnedOff: "Đã tắt chế độ chỉ VIP mới có thể sử dụng bot",
-      turnedOnNoti: "Đã bật thông báo khi người dùng không phải VIP sử dụng bot",
-      turnedOffNoti: "Đã tắt thông báo khi người dùng không phải VIP sử dụng bot"
+    fr: {
+      turnedOn: "✅ | Mode VIP seulement activé",
+      turnedOff: "✅ | Mode VIP seulement désactivé",
+      turnedOnNoti: "✅ | Notification activée pour les utilisateurs non VIP",
+      turnedOffNoti: "✅ | Notification désactivée pour les utilisateurs non VIP"
     },
     en: {
       turnedOn: "Turned on the mode only VIP can use bot",
@@ -58,8 +58,7 @@ module.exports = {
     if (isSetNoti) {
       config.hideNotiMessage.vipOnly = !value;
       message.reply(getLang(value ? "turnedOnNoti" : "turnedOffNoti"));
-    }
-    else {
+    } else {
       config.vipOnly.enable = value;
       message.reply(getLang(value ? "turnedOn" : "turnedOff"));
     }
